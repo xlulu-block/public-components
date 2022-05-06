60c65addd17fa977b153c6bd205482d7cf5d60e2
suwenlong <958089904@qq.com>
2022-05-06 14:08:22 +0800

    初始化项目

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-table></a-table>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {
+    "a-table": () => import("@/components/table"),
+  },
+  data() {
+    return {
+      column: [],
+    };
+  },
+};
+</script>
